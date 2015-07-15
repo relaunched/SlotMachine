87bcca5a2dd5ab2ed00bda53dd8af13384a089ff
Matthew Hurewitz <Matthew.Hurewitz@bestbuy.com>
2015-07-15 11:06:02 -0500

    Manually added subView

--- SlotMachine/ViewController.swift
+++ SlotMachine/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var firstContainer:UIView!
+    var secondContainer:UIView!
+    var thirdContainer:UIView!
+    var fourthContainer:UIView!
+    
+    let kMarginForView:CGFloat = 10.0
+    let kSixth:CGFloat = 1.0/6.0
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,8 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    func setupContainerViews() {
+        firstContainer = UIView(frame: CGRect(x: self.view.bounds.origin.x + kMarginForView, y: self.view.bounds.origin.y, width: self.bounds.width - (kMarginForView * 2), height: self.view.bounds.height * kSixth))
+    }
 }
 
